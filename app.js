99f988b4a4eed3824a42f54342294f0f71ac443f
Esty-8 Bechian <x4d0cil4@students.codeinstitute.net>
2024-08-13 14:13:19 +0000

    Added the images in an array

--- app.js
+++ app.js
@@ -1,8 +1,62 @@
 // the function to flip the cards
-const imageCards = document.querySelectorAll('.card');
+// const imageCards = document.querySelectorAll('.card');
 
-function flipImage() {
-   this.classList.toggle("flip");
-}
+// function flipImage() {
+//    this.classList.toggle("flip");
+// }
 
-imageCards.forEach(card => card.addEventListener('click', flipImage))
+// imageCards.forEach(card => card.addEventListener('click', flipImage))
+
+
+const arrayCards = [
+   {
+      name: 'batman',
+      img: '//assets/images/batman.png'
+   }, 
+   {
+      name: 'batwoman',
+      img: '//assets/images/batwoman.png'
+   }, 
+   {
+      name: 'harley',
+      img: '//assets/images/Harley.png'
+   }, 
+   {
+      name: 'joker',
+      img: '//assets/images/joker.png'
+   }, 
+   {
+      name: 'superman',
+      img: '//assets/images/superman.png'
+   }, 
+   {
+      name: 'wonderwoman',
+      img: '//assets/images/wonderwoman.png'
+   }, 
+   {
+      name: 'batman',
+      img: '//assets/images/batman.png'
+   }, 
+   {
+      name: 'batwoman',
+      img: '//assets/images/batwoman.png'
+   }, 
+   {
+      name: 'harley',
+      img: '//assets/images/Harley.png'
+   }, 
+   {
+      name: 'joker',
+      img: '//assets/images/joker.png'
+   }, 
+   {
+      name: 'superman',
+      img: '//assets/images/superman.png'
+   }, 
+   {
+      name: 'wonderwoman',
+      img: '//assets/images/wonderwoman.png'
+   }
+]
+
+console.log(arrayCards)
